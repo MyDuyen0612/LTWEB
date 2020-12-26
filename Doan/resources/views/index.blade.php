0df1a02582149f8812404ee3a14111d7b4533538
My Duyen <dh51704984@student.stu.edu.vn>
2020-12-26 13:18:14 +0700

    uppdae  gio hang

--- Doan/resources/views/index.blade.php
+++ Doan/resources/views/index.blade.php
@@ -140,7 +140,6 @@
                         if ($button.hasClass('inc')) {
                         var newVal = parseFloat(oldValue) + 1;
                         } else {
-                            // Don't allow decrementing below zero
                         if (oldValue > 0) {
                             var newVal = parseFloat(oldValue) - 1;
                             } else {
@@ -167,7 +166,6 @@
                         if ($button.hasClass('inc')) {
                         var newVal = parseFloat(oldValue) + 1;
                         } else {
-                            // Don't allow decrementing below zero
                         if (oldValue > 0) {
                             var newVal = parseFloat(oldValue) - 1;
                             } else {
